f070f466ae2cfdee17bf76656176d5b8a1731f36
Kevin Quizhpi <kevin.quizhpi@live.com>
2017-10-01 22:50:39 -0400

    Lab 3

---
+++ project 3/nlfilt.m
@@ -0,0 +1,16 @@
+function y = nlfilt(gamme, d, x, type)
+
+% Gamma = M-dimensionals vector of reflection coefficients
+%     d = (M+1)-dimensional vector of ladder coefficients
+%     x = input signal vector
+%     y = output signal vecor, same size as x
+%
+%  type = 'n', 's', for normalized or standard realization
+
+
+
+
+
+end
+
+
